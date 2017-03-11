3d5a82421ecdba302c55cff23fbe4e9700f2cda1
hugo2410 <hugo.birch@epfl.ch>
2017-03-11 11:45:24 +0100

    Update main.c

--- main.c
+++ main.c
@@ -6,19 +6,24 @@
 int main (int argc , char argv[])
 {
 	FILE *fichier_lecture;
-	
+	char tab[100];
+	typedef struct fourmiliere FOURMILIERE;
+	struct fourmiliere
+	{
+		int tab_info_fourmilliere[6];
+		int tab_o[][];
+		int tab_g[][];
+	}
+
 	if (argv[1]=='test')
 	{
 		fichier_lecture = fopen(argv[2], "rb");
 		
-		
-	
-	
-	
-	
-	
-	
-	
-	
-	
+		while(( fgets(tab,90,fichier_lecture)) )
+		{
+			if(tab[0]=='#'|| tab[0]=='\n'|| tab[0]=='\r')
+				continue;
+
+			sscanf("%d %d %d %d %d %d", tab2[i][0],   
+
 }
