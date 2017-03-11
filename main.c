362d0d6ab514c3b0bab4cb260fcbfb07529d9c20
hugo2410 <hugo.birch@epfl.ch>
2017-03-11 11:28:31 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "model.c"
+#include "model.h"
+
+int main (int argc , char argv[])
+{
+	FILE *fichier_lecture;
+	
+	if (argv[1]=='test')
+	{
+		fichier_lecture = fopen(argv[2], "rb");
+		
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ model.c
@@ -0,0 +1,6 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "model.h"
+#include "error.h"
+
+void verif_model(
